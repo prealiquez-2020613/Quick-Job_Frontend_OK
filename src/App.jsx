3d462b6567c0134ce7cddeb368b5fa4b8edd5b38
prealiquez-2020613 <prealiquez-2020613@kinal.edu.gt>
2025-06-15 15:47:04 -0600

    Se actualiza el componente 'App.jsx'

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { routes } from "./routes.jsx"
+import './App.css'
+
+function App() {
+  const elements = useRoutes(routes)
+  return (
+    <>
+      {elements}
+      <Toaster position='bottom-right' reverserOrder={false}/>
+    </>
+  )
+}
+
+export default App
