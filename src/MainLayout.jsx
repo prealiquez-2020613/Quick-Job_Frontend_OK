e1cc9a02e54eaa5348052f4c11aa2b2f9af63687
aperez-2020067 <aperez-2020067@kinal.edu.gt>
2025-06-16 09:02:52 -0600

    Se agrega Navbar funcional

---
+++ src/MainLayout.jsx
@@ -0,0 +1,17 @@
+
+import { Navbar } from './components/Navbar.jsx'
+import { Outlet } from "react-router-dom"
+
+export const MainLayout = () => {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Navbar a la izquierda */}
+      <Navbar />
+
+      {/* Contenido principal */}
+      <main className="flex-1 p-6 overflow-y-auto">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,32 @@
+// src/routes.jsx
 import { Login } from "./components/Login";
+import { Register } from "./components/Regsiter";
 import { WorkersPage } from "./pages/WorkersPage";
 import { WorkerInfo } from "./components/user/WorkerInfo";
-import { Register } from "./components/Regsiter";
+import { MainLayout } from "./MainLayout";
+
 
 export const routes = [
-    {path:'/',element:<Login/>},
-    {path:'/home',element:<WorkersPage/>},
-    {path:'/worker/:workerId', element:<WorkerInfo/>},
-    { path: '/register', element: <Register /> },
+  {
+    path: '/',
+    element: <Login /> // SIN NAVBAR
+  },
+  {
+    path: '/register',
+    element: <Register /> // SIN NAVBAR
+  },
+  {
+    path: '/',
+    element: <MainLayout />, // CON NAVBAR
+    children: [
+      {
+        path: '/home',
+        element: <WorkersPage />
+      },
+      {
+        path: '/worker/:workerId',
+        element: <WorkerInfo />
+      }
+    ]
+  }
 ]
