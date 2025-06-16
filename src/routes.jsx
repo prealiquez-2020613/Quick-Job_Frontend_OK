0ec3be00aa3b16227212324a2b19f931f5d205d2
aperez-2020067 <aperez-2020067@kinal.edu.gt>
2025-06-15 21:35:07 -0600

    Se agregra Register funcional

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import { Login } from "./components/Login";
 import { WorkersPage } from "./pages/WorkersPage";
 import { WorkerInfo } from "./components/user/WorkerInfo";
+import { Register } from "./components/Regsiter";
 
 export const routes = [
     {path:'/',element:<Login/>},
     {path:'/home',element:<WorkersPage/>},
     {path:'/worker/:workerId', element:<WorkerInfo/>},
+    { path: '/register', element: <Register /> },
 ]
