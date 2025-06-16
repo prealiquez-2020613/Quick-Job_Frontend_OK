67e98c51b908703c0e55c264d465bbf1d794a042
prealiquez-2020613 <prealiquez-2020613@kinal.edu.gt>
2025-06-15 20:10:08 -0600

    Se agrega ruta de 'WorkersPage' a 'routes.jsx'

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,7 @@
 import { Login } from "./components/Login";
+import { WorkersPage } from "./pages/WorkersPage";
 
 export const routes = [
     {path:'/',element:<Login/>},
+    {path:'/home',element:<WorkersPage/>},
 ]
