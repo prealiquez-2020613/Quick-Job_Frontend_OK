bae3a09a0b909992776ee7a9b33ebdb193e333a7
jalvarado-2021353 <jalvarado-2021353@kinal.edu.gt>
2025-06-17 19:17:52 -0600

    Se crea el componente 'SearchPage'

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import SearchWorkers from '../components/user/SearchWorkers';
+
+const SearchPage = () => {
+  const [departments, setDepartments] = useState([]);
+
+  useEffect(() => {
+    const departamentos = [
+    'Alta Verapaz', 'Baja Verapaz', 'Chimaltenango', 'Chiquimula', 'Escuintla', 'Guatemala',
+    'Huehuetenango', 'Izabal', 'Jalapa', 'Jutiapa', 'Petén', 'Quetzaltenango', 'Quiché',
+    'Retalhuleu', 'Sacatepéquez', 'San Marcos', 'Santa Rosa', 'Sololá', 'Suchitepéquez',
+    'Totonicapán', 'Zacapa'
+  ];
+    setDepartments(departamentos);
+  }, []);
+
+  return (
+    <div className="search-page">
+      <h1>Búsqueda de Trabajadores</h1>
+      <SearchWorkers departments={departments} />
+    </div>
+  );
+};
+
+export default SearchPage;
