c82db56e8d9678c75a374d23ed8a943ce87491fa
prealiquez-2020613 <prealiquez-2020613@kinal.edu.gt>
2025-06-17 19:49:08 -0600

    Se crea el componente 'ReviewList'

---
+++ src/components/review/ReviewList.jsx
@@ -0,0 +1,20 @@
+import ReviewCard from './ReviewCard';
+
+const ReviewsList = ({ reviews }) => {
+  return (
+    <div className="w-full mt-6">
+      <h3 className="text-2xl font-bold mb-4">Reseñas de este trabajador</h3>
+      <div className="flex flex-wrap gap-4">
+        {reviews.length === 0 ? (
+          <p>No hay reseñas para mostrar.</p>
+        ) : (
+          reviews.map((review, index) => (
+            <ReviewCard key={index} review={review} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ReviewsList;
