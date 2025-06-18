75c96407c9b1c29c7f3571e862b82141a79ac2e0
prealiquez-2020613 <prealiquez-2020613@kinal.edu.gt>
2025-06-17 21:49:38 -0600

    Se implementan mensajes personalizados en 'useCreateReview'

--- src/shared/hooks/review/useCreateReview.jsx
+++ src/shared/hooks/review/useCreateReview.jsx
@@ -13,8 +13,16 @@ export const useCreateReview = () => {
     const response = await createReview(payload);
     
     if (response.error) {
-      setError(response?.err?.response?.data?.message || 'Error al crear la reseña');
-      toast.error('Error al enviar la reseña');
+      setError(response?.err?.response?.data?.message || 'Error al enviar la reseña');
+
+      if (response?.err?.response?.data?.message === 'You already reviewed this user') {
+        toast.error('Ya reseñaste a este usuario');
+      } else if (response?.err?.response?.data?.message === 'You cannot review someone you have not worked with') {
+        toast.error('No puedes reseñar a alguien con quien no has trabajado');
+      } else {
+        toast.error('Error al enviar la reseña');
+      }
+
     } else {
       toast.success('¡Reseña enviada con éxito!');
     }
